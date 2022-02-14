f46f75f2665ea0fc0faac33e65ee2ea8999224e6
Eduard Bargués <88039083+lucky-edu@users.noreply.github.com>
2022-02-14 11:34:44 +0100

    ci: LANZ-1654 add cicd workflows (#11)

--- versions.tf
+++ versions.tf
@@ -1,10 +1,9 @@
 terraform {
   required_version = "~> 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = "~>3.74"
     }
   }
 }
